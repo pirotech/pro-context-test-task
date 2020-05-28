15b6a6f57646704f125941c80f93eb841a59ec1c
Sergey <serjioini@gmail.com>
2020-05-28 15:07:26 +0300

    Boilerplate created

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  min-height: 100vh;
+}
+
+html,
+body,
+div,
+header,
+main,
+ul,
+li,
+ol,
+p,
+span,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+button,
+input,
+textarea {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  box-sizing: border-box;
+  outline: none;
+}
+
+input, textarea {
+  border: 1px solid #dddddd;
+  border-radius: 2px;
+  padding: 8px 12px;
+  font-size: 14px;
+}
+button {
+  border: 1px solid #ccc;
+  border-radius: 2px;
+  padding: 8px 12px;
+  background-color: white;
+  cursor: pointer;
+  font-size: 14px;
+}
+</style>
---
+++ src/views/MainPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="main-page">
+    main page
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainPage'
+}
+</script>
+
+<style lang="scss">
+.main-page {
+  width: 100%;
+  min-height: 100vh;
+}
+</style>
